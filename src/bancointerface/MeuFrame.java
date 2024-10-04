575d7a4a734566551ed57ac36e843631bf07ff31
JuannMaicon <jmaicon308@gmail.com>
2024-10-04 10:11:02 -0300

    JFrame

--- src/bancointerface/MeuFrame.java
+++ src/bancointerface/MeuFrame.java
@@ -8,37 +8,46 @@ import javax.swing.JFrame;
 
 public class MeuFrame extends JFrame implements ActionListener {
 	JButton meuBotao;
-	
-	public MeuFrame() {
-		//MeuBotao
-		meuBotao = new JButton("Meu botão");
-		meuBotao.setBounds(20,20,100,30);
-		meuBotao.addActionListener(this);
-		MeuActionListener listener =new MeuActionListener();
-		meuBotao.addActionListener(listener);
 
-		//Frame
+	public MeuFrame() {
+		// MeuBotao
+		meuBotao = new JButton("Meu botão");
+		meuBotao.setBounds(20, 20, 100, 30);
+		meuBotao.addActionListener(this);
+		MeuActionListener listener = new MeuActionListener();
+		meuBotao.addActionListener(listener);
+		
+		meuBotao.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Cliquei No Botão 3ª Vez");
+
+			}
+		});
+
+		// Frame
 		getContentPane().setLayout(null);
 		getContentPane().add(meuBotao);
-		setBounds(100,100,300,300);
+		setBounds(100, 100, 300, 300);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Cliquei No Botão");
 	}
-	
+
 	public static void main(String[] args) {
 		new MeuFrame();
 	}
-	//Classe interna
+
+	// Classe interna
 	class MeuActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Cliquei No Botão De Novo");
 		}
 	}
-	
 
 }
